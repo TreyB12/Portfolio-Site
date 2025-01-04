f5c5df78c6bcaf4b1b64054696c8009664a1408d
TreyB12 <trey11790@gmail.com>
2025-01-04 16:24:49 -0600

    Portfolio-Website-1
    First commit after first night of working on the portfolio website

---
+++ Portfolio-site-React/src/Header.jsx
@@ -0,0 +1,20 @@
+
+export default function Header () {
+    return (
+
+    <header>
+        <h2 id="header-name"><a href="">Trey Boyd</a></h2>
+
+        <section className="top-links">
+
+       <a href="about">About</a>
+        <a href="projects">Projects</a>
+        <a href="contact">Contact</a>
+
+        </section>
+
+    </header>
+
+    )
+    
+}
---
+++ Portfolio-site-React/src/main.jsx
@@ -0,0 +1,26 @@
+import { createRoot } from 'react-dom/client'
+import Header from "./Header.jsx"
+import HeaderBody from "./Header-Body.jsx"
+import Skills from "./Skills.jsx"
+
+
+createRoot(document.getElementById('root')).render(
+  <>
+  <Header />
+  <HeaderBody />
+  <Skills />
+  <Main />
+ </>
+)
+
+function Main () {
+  return (
+
+<>
+
+
+</>
+
+
+  )
+}
